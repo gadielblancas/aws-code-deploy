059eb989bf133f564d50c976446558164c5c3b99
gadielblancas <gadiel.blancas@gmail.com>
2017-03-19 10:30:29 -0600

    aws-code-deploy
    primer commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<title>hola mundo</title>
+</head>
+<style type="text/css">
+
+body
+{background-color:lightgray; font-size:19px;}
+body div
+{color:white; width:100%; text-align:center;}
+
+</style>
+<body>
+  <div>hello world from github!</div>
+</body>
+</html>
